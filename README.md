237e1aeac2a8a6418cdeed8f576dcc97e490b35c
JSH32 <rudnik7000@gmail.com>
2023-07-13 09:01:25 -0500

    Init config

---
+++ README.md
@@ -0,0 +1,7 @@
+# Windows dotfiles
+These are my windows dots. This includes my keybinds, yasb, and komorebi configs.
+
+## Software
+* [Komorebi master branch](https://github.com/LGUG2Z/komorebi)
+* [My fork of yasb](https://github.com/jsh32/yasb)
+* [WHKD](https://github.com/LGUG2Z/whkd)
---
+++ komorebi.ps1
@@ -0,0 +1,23 @@
+$YasbPath = "$Env:USERPROFILE/Documents/Projects/yasb-py"
+
+Write-Output "Killing processes"
+taskkill /IM komorebi.exe /F
+
+Set-Location -Path $PSScriptRoot
+$PidFile = "$PSScriptRoot/yasb_pid.txt"
+
+if (Test-Path $PidFile) {
+    $YasbPids = Get-Content -Path $PidFile -Raw
+
+    foreach ($line in $YasbPids) {
+        if (-not $line -or $line -ne "") {
+            taskkill /F /PID $line
+        }
+    }
+
+    Remove-Item $PidFile
+}
+
+Write-Output "Starting processes"
+komorebic start -c $Env:USERPROFILE/komorebi.json
+Start-Process -WindowStyle hidden -FilePath python -ArgumentList $YasbPath/src/main.py
